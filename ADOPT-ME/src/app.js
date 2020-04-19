b23da88fe392fe264b0278569fceb2173aa789b2
mcsheakp <kevinmcshea@gmail.com>
2020-04-19 12:31:21 -0700

    finished up react hooks section

--- ADOPT-ME/src/app.js
+++ ADOPT-ME/src/app.js
@@ -1,29 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./searchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Ziggy",
-  //     animal: "Dog",
-  //     breed: "Daucshaund",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Laggy",
-  //     animal: "Dog",
-  //     breed: "Buttdog",
-  //   }),
-  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Stray" }),
-  // ]);
-
   return (
     <div>
       <h1 id="something-importants">Adopt Me!</h1>
-      <Pet name="Ziggy" animal="Dog" breed="Dauchaund" />
-      <Pet name="Doink" animal="Cat" breed="Mix" />
-      <Pet name="ButtButt" animal="Dog" breed="Lab" />
+      <SearchParams></SearchParams>
     </div>
   );
 };
